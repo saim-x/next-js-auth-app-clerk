bf271e948fe27cf79efe86a4b49f41ef6a3cf252
saim-x <x_aci@hotmail.com>
2024-08-10 23:47:42 +0500

    test

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
     <div className='flex flex-col p-4 mt-4 w-full '>
       <h1 className='text-2xl font-semibold text-gray-800'>Home</h1>
       <p className='mt-2 text-gray-600'>
-        This is a simple authentication app built with Next.js and Clerk.
+        This is a simple authentication app built with Next.js and Clerk. Happy Coding!
       </p>
     </div>);
 }
