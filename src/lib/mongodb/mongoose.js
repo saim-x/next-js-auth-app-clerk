ec8486b650447755ba1dbca604e64cc57e426451
saim-x <x_aci@hotmail.com>
2024-08-10 23:15:30 +0500

    db name updated

--- src/lib/mongodb/mongoose.js
+++ src/lib/mongodb/mongoose.js
@@ -8,7 +8,7 @@ export const connect = async () => {
     }
     try{
         await mongoose.connect(process.env.MONGODB_URI, {
-            dbName:"next auth app",
+            dbName:"nextjs-auth-app",
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
