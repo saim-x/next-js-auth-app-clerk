49e1d702247284fbda0766e69b2c8da3705e272f
saim-x <x_aci@hotmail.com>
2024-08-10 23:26:37 +0500

    another try

--- src/lib/models/user.model.js
+++ src/lib/models/user.model.js
@@ -13,11 +13,9 @@ const userSchema = new mongoose.Schema({
     },
     firstName: {
         type: String,
-        required: true,
     },
     lastName: {
         type: String,
-        required: true,
     },
     username: {
         type: String,
