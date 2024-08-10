5e10f453bfdf9febf0c8b7a529c2adbfbb85e8c5
saim-x <x_aci@hotmail.com>
2024-08-10 23:18:36 +0500

    another try

--- src/lib/mongodb/mongoose.js
+++ src/lib/mongodb/mongoose.js
@@ -8,7 +8,7 @@ export const connect = async () => {
     }
     try{
         await mongoose.connect(process.env.MONGODB_URI, {
-            dbName:"nextjs-auth-app",
+            dbName:"next-auth-app",
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
